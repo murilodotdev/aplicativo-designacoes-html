1ca701e1b3c05bd50b9fc275f7a7c10e633f00b9
murilodotdev <murilo.for.work@gmail.com>
2025-05-18 13:24:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("botao-login")
+button.addEventListener("click", (e) => {
+    let nomeUsuario = document.getElementById("input-nome")
+    let senha = document.getElementById("input-senha")
+    console.log(nomeUsuario, senha)
+})
